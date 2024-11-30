43aa3bcb62a101626e075b621b18795e99c9cdae
r-yvan <yvankiliye.rubuto@gmail.com>
2024-11-30 17:43:32 +0200

    Commit

--- src/IOJava/Main.java
+++ src/IOJava/Main.java
@@ -5,20 +5,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    FileInputStream src = new FileInputStream("/media/rubuto-yvan/Personal Disk/Personal Skills Development/Coding & Development/Java/Projects/java-class-practice/src/IOJava/LoremInput.txt");
+    FileOutputStream dest = new FileOutputStream("/media/rubuto-yvan/Personal Disk/Personal Skills Development/Coding & Development/Java/Projects/java-class-practice/src/IOJava/LoremOutput.txt");
+    
     try {
-      FileInputStream src = new FileInputStream("/media/rubuto-yvan/Personal Disk/Personal Skills Development/Coding & Development/Java/Projects/java-class-practice/src/IOJava/LoremInput.txt");
-      FileOutputStream dest = new FileOutputStream("/media/rubuto-yvan/Personal Disk/Personal Skills Development/Coding & Development/Java/Projects/java-class-practice/src/IOJava/LoremOutput.txt");
-      
       int singleByte;
       while ((singleByte = src.read()) != -1) {
         dest.write(singleByte);
       }
-      
+    } catch (IOException error) {
+      System.out.println("An Error occured: " + error.getMessage());
+    } finally {
       src.close();
       dest.close();
-    } catch (IOException error) {
-      error.getMessage();
     }
   }
 }
